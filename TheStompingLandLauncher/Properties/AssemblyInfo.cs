79f14b52b10a2a039ec74138325c05a888c2b1ce
Christian Moritz <chrmoritz@gmail.com>
2014-06-02 16:21:59 +0200

    added missing files

--- TheStompingLandLauncher/Properties/AssemblyInfo.cs
+++ TheStompingLandLauncher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("thestompinglandlauncher")]
+[assembly: AssemblyTitle("TheStompingLandLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("thestompinglandlauncher")]
+[assembly: AssemblyProduct("TheStompingLandLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
